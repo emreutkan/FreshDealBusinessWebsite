c5112db569dd1ef71c1a42d432564737c5e91491
emreutkan <127414322+emreutkan@users.noreply.github.com>
2025-01-14 02:39:52 +0300

    api

--- src/redux/Api/apiService.ts
+++ src/redux/Api/apiService.ts
@@ -1,3 +1,3 @@
 // api/apiService.ts
-// export const API_BASE_URL = 'https://freshdealapi-fkfaajfaffh4c0ex.uksouth-01.azurewebsites.net/v1';
-export const API_BASE_URL = 'http://192.168.1.3:8181/v1';
+export const API_BASE_URL = 'https://freshdealapi-fkfaajfaffh4c0ex.uksouth-01.azurewebsites.net/v1';
+// export const API_BASE_URL = 'http://192.168.1.3:8181/v1';
