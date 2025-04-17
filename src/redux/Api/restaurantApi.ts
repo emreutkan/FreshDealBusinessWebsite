4def597f9f693c37571a726263e6315d90610479
emreutkan <127414322+emreutkan@users.noreply.github.com>
2025-04-18 00:09:39 +0300

    checkpoint
    update restaurant details works as intended

--- src/redux/Api/restaurantApi.ts
+++ src/redux/Api/restaurantApi.ts
@@ -1,10 +1,20 @@
 import axios from 'axios';
 import {API_BASE_URL} from "./apiService.ts";
 const RESTAURANT_ENDPOINT = `${API_BASE_URL}/restaurants`;
-const GET_UPLOADED_FILE_API_ENDPOINT = `${API_BASE_URL}/uploads`; // Needs filename appended
+const GET_UPLOADED_FILE_API_ENDPOINT = `${API_BASE_URL}/uploads`;
 
 export const addRestaurantAPI = async (formData: FormData, token: string) => {
     const response = await axios.post(RESTAURANT_ENDPOINT, formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'Authorization': `Bearer ${token}`,
+        },
+    });
+    return response.data;
+};
+
+export const updateRestaurantAPI = async (restaurantId: string, formData: FormData, token: string) => {
+    const response = await axios.put(`${RESTAURANT_ENDPOINT}/${restaurantId}`, formData, {
         headers: {
             'Content-Type': 'multipart/form-data',
             'Authorization': `Bearer ${token}`,
@@ -30,8 +40,6 @@ export const deleteRestaurantAPI = async (restaurantId: string, token: string) =
     });
     return response.data;
 };
-
-
 
 export const getUploadedFileURLAPI = (filename: string) => {
     const url = `${GET_UPLOADED_FILE_API_ENDPOINT}/${filename}`;
